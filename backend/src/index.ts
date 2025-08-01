0f4c4750eb61e09e4913a76b10d2620ab9e3ec18
zisshhh <zisshhh100xdevs@gmail.com>
2025-08-02 00:41:40 +0530

    port added to env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import cors from "cors"
 import { rootRouter } from "./routes";
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT
 
 app.use("/api/v1", rootRouter);
 
@@ -13,4 +16,4 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(3000);
+app.listen(PORT);
